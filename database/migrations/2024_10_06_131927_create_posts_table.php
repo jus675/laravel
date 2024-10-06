3433af638a8a2b7f8818e3f5ea8449003d67fa91
Justine Denise Hernandez <114111385+jus675@users.noreply.github.com>
2024-10-06 13:25:55 +0000

    Create Category and Post Models

--- database/migrations/2024_10_06_131927_create_posts_table.php
+++ database/migrations/2024_10_06_131927_create_posts_table.php
@@ -11,9 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('title');
+            $table->text('text');
+            $table->foreignId('category_id')->constrained();
             $table->timestamps();
         });
     }
@@ -23,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('posts');
     }
 };
